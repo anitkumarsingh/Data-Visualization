b44bb13e5a0023ef290cf64b893af6f37fec8a66
Anit Kumar <anikumarsingh99@gmail.com>
2019-05-13 20:32:38 +0530

    added socail medial link

--- src/Component/Footer/index.js
+++ src/Component/Footer/index.js
@@ -10,10 +10,10 @@ const Footers = () => (
   <Footer className="app-footer bg-white">
     <div className="footer-inner-v2">
       <ul className="footer-social-list">
-        <li><a href='/'><Icon type="google" /></a></li>
-        <li><a href='/'><Icon type="facebook" /></a></li>
-        <li><a href='/'><Icon type="twitter" /></a></li>
-        <li><a href='/'><Icon type="instagram" /></a></li>
+        <li><a href='https://github.com/anitkumarsingh' target="_blank" rel="noopener noreferrer"><Icon type="github" /></a></li>
+        <li><a href='https://www.linkedin.com/in/anit-kumar' target="_blank" rel="noopener noreferrer"><Icon type="linkedin"/></a></li>
+        <li><a href='https://anitkumar-portfolio.firebaseapp.com/' target="_blank" rel="noopener noreferrer"><Icon type="link" /></a></li>
+        <li><a href='/'><Icon type="codepen-circle" /></a></li>
       </ul>
       <div className="footer-copyright">
         <span>© {year}, Design & Developed By Anit Kumar </span>
